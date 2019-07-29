3ed76e67ed030ff7935116216a645c50371fe960
fentonhaslam <fentonhaslam@gmail.com>
2019-07-29 16:25:30 +0100

    added fail error - maximum balance

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -5,17 +5,17 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
-  describe '#top_up' do
+  describe "#top_up" do
     it { is_expected.to respond_to(:top_up).with(1).argument }
-    
 
-    it 'tops up the balance' do
-      expect{subject.top_up(1) }.to change{subject.balance}.by (1)
+    it "tops up the balance" do
+      expect { subject.top_up(1) }.to change { subject.balance }.by (1)
     end
 
+    it "raises an error if the maximum balance is exceeded" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
-
-
+  end
 end
-
-
