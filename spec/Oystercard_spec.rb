d1399d97b6bbeab668f263ef10329afb2c5d56bb
Kat <vampirebatkat@gmail.com>
2019-07-29 16:55:46 +0100

    deduct method & test

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -18,4 +18,13 @@ describe Oystercard do
       expect { subject.top_up(1) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
   end
+
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+  end
+
+    it 'deducts an amount from the balance' do
+      subject.top_up(20)
+      expect { subject.deduct(5) }.to change{ subject.balance }.by (-5)
+    end
 end
